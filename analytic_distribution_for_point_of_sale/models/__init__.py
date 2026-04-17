d1b7c88d83544333df84f1ab3b25168a335162e3
mohamed maati <hamadamoaty092@gmail.com>
2026-04-17 17:28:52 +0300

    8523dfghjgfd

--- analytic_distribution_for_point_of_sale/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import pos_config_analytic_line
-from . import pos_analytic_history_wizard
-from . import pos_config
-from . import pos_order
-from . import pos_session
-from . import res_config_settings
