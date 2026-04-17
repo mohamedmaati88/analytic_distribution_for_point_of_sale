444fbd7fd24f782fe0b4c05dcb6667e51f05832d
mohamed maati <hamadamoaty092@gmail.com>
2026-04-17 16:00:04 +0300

    546jhgfds

---
+++ analytic_distribution_for_point_of_sale/models/pos_config_analytic_line.py
@@ -0,0 +1,46 @@
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class PosConfigAnalyticLine(models.Model):
+    _name = 'pos.config.analytic.line'
+    _description = 'POS Analytic Distribution Line'
+    _order = 'id'
+
+    config_id = fields.Many2one(
+        comodel_name='pos.config',
+        required=True,
+        ondelete='cascade',
+    )
+    analytic_account_id = fields.Many2one(
+        comodel_name='account.analytic.account',
+        string='Analytic Account',
+        required=True,
+    )
+    percentage = fields.Float(
+        string='%',
+        default=100.0,
+        digits=(5, 2),
+    )
+
+    @api.constrains('percentage')
+    def _check_percentage(self):
+        for rec in self:
+            if not (0 < rec.percentage <= 100):
+                raise ValidationError(
+                    self.env._('Percentage must be between 0.01 and 100.')
+                )
+
+    @api.constrains('analytic_account_id', 'config_id')
+    def _check_unique_analytic_account(self):
+        for rec in self:
+            duplicate = self.search([
+                ('config_id', '=', rec.config_id.id),
+                ('analytic_account_id', '=', rec.analytic_account_id.id),
+                ('id', '!=', rec.id),
+            ], limit=1)
+            if duplicate:
+                raise ValidationError(
+                    self.env._('Analytic account "%s" is already added to this POS configuration. Duplicate entries are not allowed.') % rec.analytic_account_id.display_name
+                )
+
